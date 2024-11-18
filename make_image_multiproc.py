f6356bdaf59b0ec490d6500b36171edacb2a495a
Dmitry <astronom.v.cube@gmail.com>
2024-11-19 01:16:42 +0300

    switching to concurrent.futures

---
+++ make_image_multiproc.py
@@ -0,0 +1,85 @@
+import os
+import logging
+import concurrent.futures
+from synthesis_utils import GlobaMultiSynth
+from tqdm import tqdm
+import itertools
+import os
+from tqdm import tqdm
+from threading import Lock
+
+###### Для заполнения ######
+observation_range = '1224'
+path_to_calib_tables = '/home/dmitry/Documents/calib_tables/2024/20240514 - 1224 - 02-02-49.json'
+directory_of_data = '/home/dmitry/14may/1224/data'
+directory_of_result = '/mnt/astro/14maytetette'
+all_tasks_file = "all_tasks.json"
+done_tasks_file = "done_tasks.json"
+flags_freq = [12960, 13720, 14480, 15240, 16760, 17520, 18280, 19040, 19800, 20560, 21320, 22080, 23400]#[12960, 13720, 23000]
+number_of_clean_iter = 200000
+
+frequencies = list(range(20))
+scans = list(range(20))
+num_threads = 10
+######                ######
+
+synthesizer = GlobaMultiSynth(directory_of_data, directory_of_result, path_to_calib_tables, number_of_clean_iter)
+synthesizer.start_procedures()
+synthesizer.start_log('synth_log')
+lock = Lock()
+
+files = sorted(os.listdir(directory_of_data))
+logging.info(f'Список файлов: {files}')
+
+list_of_freqs = synthesizer.indicate_observation_range(observation_range)
+synthesizer.create_places(directory_of_result, list_of_freqs, flags_freq)
+
+def task_wrapper(task):
+    file_name, frequency, scan_number = task
+    try:
+        synthesizer.image_maker(file_name, frequency, scan_number)
+        return task, True
+    except Exception as e:
+        print(f"Error processing {task}: {e}")
+        return task, False
+
+def main(files, frequencies, scans, num_threads, all_tasks_file, done_tasks_file):
+    os.system('python -m casaconfig --update-all')
+
+    all_tasks = [
+        (file, freq, scan)
+        for file, freq, scan in itertools.product(files, frequencies, scans)
+        if list_of_freqs[freq] not in flags_freq
+    ]
+
+    saved_all_tasks, saved_done_tasks = synthesizer.load_progress(all_tasks_file, done_tasks_file)
+
+    # Если сохранённый список задач совпадает с текущим, продолжаем с того места, где остановились
+    if set(tuple(task) for task in saved_all_tasks) == set(tuple(task) for task in all_tasks):
+        done_tasks = saved_done_tasks
+    else:
+        # Иначе начинаем заново и сохраняем прогресс
+        done_tasks = set()
+
+    remaining_tasks = [task for task in all_tasks if task not in done_tasks]
+
+    with tqdm(total=len(list(all_tasks)), desc="Progress", bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} tasks") as pbar:
+        pbar.update(len(list(done_tasks)))
+
+        with concurrent.futures.ProcessPoolExecutor(max_workers=num_threads) as executor:
+            future_to_task = {executor.submit(task_wrapper, task): task for task in remaining_tasks}
+            for future in concurrent.futures.as_completed(future_to_task):
+                task, success = future.result()
+                if success:
+                    done_tasks.add(task)
+                    with lock:
+                        synthesizer.save_progress(all_tasks_file, done_tasks_file, all_tasks, list(done_tasks))
+                    pbar.update(1)
+
+    synthesizer.remove_progress(all_tasks_file, done_tasks_file)
+
+if __name__ == "__main__":
+    main(files, frequencies, scans, num_threads, all_tasks_file, done_tasks_file)
+
+
+
--- synthesis_utils.py
+++ synthesis_utils.py
@@ -1,12 +1,18 @@
 import os
-import multiprocessing
 import psutil
 import logging
 import time
+import json
+import re
+import shutil
 
 class GlobaMultiSynth():
 
-    def __init__(self) -> None:
+    def __init__(self, directory_of_data, directory_of_result, path_to_calib_tables, number_of_clean_iter) -> None:
+        self.directory_of_data = directory_of_data
+        self.directory_of_result = directory_of_result
+        self.path_to_calib_tables = path_to_calib_tables
+        self.number_of_clean_iter = number_of_clean_iter
         self.list_of_freqs_0306 = [2800, 3000, 3200, 3400, 3600, 3800, 4000, 4200, 4400, 4600, 4800, 5000, 5200, 5400, 5600, 5800]
         self.list_of_freqs_0612 = [6000, 6400, 6800, 7200, 7600, 8000, 8400, 8800, 9200, 9600, 10000, 10400, 10800, 11200, 11600, 12000]
         self.list_of_freqs_1224 = [12200, 12960, 13720, 14480, 15240, 16000, 16760, 17520, 18280, 19040, 19800, 20560, 21320, 22080, 23000, 23400]
@@ -15,7 +21,7 @@ class GlobaMultiSynth():
     def start_log(name_file : str):
         """Инициализация ```.log``` файла
         Args:
-            name_file (str): название файла
+            name_file (str): название файла без расширения
         """
         logging.basicConfig(filename = f'{name_file}.log',  filemode='a', level = logging.INFO, format = '%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', encoding = "UTF-8")
         # encoding = "UTF-8"
@@ -30,27 +36,18 @@ class GlobaMultiSynth():
         print(info_msg)
         logging.info(info_msg)
 
-    def transform_timestamp(self, timestamp: str):
-        """Трансформация имени файла в имя маски
-        Args:
-            timestamp (str): имя файла
-        """
-        return f"{timestamp[:4]}-{timestamp[4:6]}-{timestamp[6:8]}T{timestamp[9:11]}:{timestamp[11:13]}:{timestamp[13:15]}"
+    @staticmethod
+    def start_procedures():
+        import os
+        N_THREADS = "1"
+        os.environ['OMP_NUM_THREADS'] = N_THREADS
+        os.environ['OPENBLAS_NUM_THREADS'] = N_THREADS
+        os.environ['MKL_NUM_THREADS'] = N_THREADS
+        os.environ['VECLIB_MAXIMUM_THREADS'] = N_THREADS
+        os.environ['NUMEXPR_NUM_THREADS'] = N_THREADS
 
-    def start_procedures(self):
-        # на случай выполнения на ssd чтобы кэш не копился
-        # os.system("tracker3 daemon -k")
-        # os.system("tracker3 daemon --list-miners-running")
-
-        multiprocessing.log_to_stderr()
-        logger = multiprocessing.get_logger()
-        logger.setLevel(logging.INFO)
-
-    def finish_procedures(self, directory_of_result, list_of_freqs):
-        # os.system("tracker3 daemon -s")
-        os.system("rm -rf casa*.log")
-        for fq in list_of_freqs:
-            os.system(f"rm -rf {directory_of_result}/{fq}/*_mask")
+        from casatasks import casalog
+        casalog.filter('SEVERE')
 
     def create_places(self, directory_of_result, list_of_freqs, flags_freq):
         try:
@@ -82,57 +79,79 @@ class GlobaMultiSynth():
         free_ram = memory_info.available / (1024 ** 2)
         return free_ram
 
+    def image_maker(self, file_of_data, frequency, scan_number):
+        """
+        Мультипроцессинговая функция по созданию радиоизображений
+        """
+        self.start_procedures()
 
+        import srhdata
+        srhfile = srhdata.open(f'{self.directory_of_data}/{file_of_data}')
 
+        from tempfile import TemporaryDirectory
+        tmpfs = TemporaryDirectory(prefix='srh_tmp_', suffix=f'_{file_of_data[9:-4]}')
+        TMPFS_DIR = tmpfs.name
+        logging.info(f'Working in {TMPFS_DIR}')
+        os.chdir(TMPFS_DIR)
 
+        start = time.time()
+        logging.info(f'{file_of_data} in process')
 
+        srhfile.makeImage(
+            path = f'{TMPFS_DIR}',
+            calibtable = self.path_to_calib_tables,
+            remove_tables = True,
+            frequency = frequency,
+            scan = scan_number,
+            average = 0,
+            RL = True,
+            clean_disk = False,
+            calibrate = False,
+            cdelt = 2.45,
+            naxis = 1024,
+            niter = self.number_of_clean_iter,
+            threshold = 75000,
+            stokes = 'RRLL',
+            deconvolver = 'multiscale',
+            scales=[0, 3]
+        )
 
+        logging.info(f'Time for {file_of_data} on freq {frequency} scan {scan_number}: {int(time.time() - start)}')
 
-    # def image_maker(self, file_of_data, freq, list_of_freqs, flags_freq, path_to_calib_tables, directory_of_data, directory_of_result, number_of_clean_iter, threshold):
-    #     """
-    #     Мультипроцессинговая функция по созданию радиоизображений для конкретной частоты
-    #     """
+        for name in srhfile.out_filenames:
+            filename = name.split('/')[-1]
+            freqs_pattern = re.compile(r'(?<=[_.])\d{4,5}(?=[_.])')
+            freqqq = int(freqs_pattern.search(name).group())
+            shutil.copy(name, f'{self.directory_of_result}/{freqqq}/{filename}')
+            logging.info(f'{filename} - OK')
 
-    #     import os
-    #     N_THREADS="1"
-    #     os.environ['OMP_NUM_THREADS'] = N_THREADS
-    #     os.environ['OPENBLAS_NUM_THREADS'] = N_THREADS
-    #     os.environ['MKL_NUM_THREADS'] = N_THREADS
-    #     os.environ['VECLIB_MAXIMUM_THREADS'] = N_THREADS
-    #     os.environ['NUMEXPR_NUM_THREADS'] = N_THREADS
+        tmpfs.cleanup()
 
-    #     from casatasks import casalog
-    #     casalog.filter('SEVERE')
-    #     import srhdata
+    def save_progress(self, all_tasks_file, done_tasks_file, all_tasks, done_tasks):
+        try:
+            with open(all_tasks_file, 'w') as f:
+                json.dump(all_tasks, f, indent=4)
+            with open(done_tasks_file, 'w') as f:
+                json.dump(list(done_tasks), f, indent=4)
+        except Exception as e:
+            logging.error(f"Error saving progress: {e}")
 
-    #     srhfile = srhdata.open(f'{directory_of_data}/{file_of_data}')
+    def load_progress(self, all_tasks_file, done_tasks_file):
+        if os.path.exists(all_tasks_file) and os.path.exists(done_tasks_file):
+            try:
+                with open(all_tasks_file, 'r') as f:
+                    all_tasks = json.load(f)
+                with open(done_tasks_file, 'r') as f:
+                    # Преобразуем список в множество кортежей, если элементы являются списками
+                    done_tasks = {tuple(task) if isinstance(task, list) else task for task in json.load(f)}
+                return all_tasks, done_tasks
+            except (json.JSONDecodeError, FileNotFoundError) as e:
+                logging.warning(f"Error loading progress: {e}")
+                return [], set()
+        else:
+            return [], set()
 
-    #     for scan in range(0, 20):
-    #         start = time.time()
-
-    #         if list_of_freqs[freq] in flags_freq:
-    #             pass
-
-    #         else:
-    #             proc_id = os.getpid()
-    #             print(f'{file_of_data} in process id: {proc_id}, frequency: {freq}')
-    #             srhfile.makeImage(
-    #                 path = f'./{directory_of_result}/{list_of_freqs[freq]}',
-    #                 calibtable = path_to_calib_tables,
-    #                 remove_tables = True,
-    #                 frequency = freq,
-    #                 scan = scan,
-    #                 average = 0,
-    #                 RL = True,
-    #                 clean_disk = True,
-    #                 calibrate = False,
-    #                 cdelt = 2.45,
-    #                 naxis = 1024,
-    #                 niter = number_of_clean_iter,
-    #                 threshold = threshold,
-    #                 stokes = 'RRLL'
-    #             )
-
-    #         finish = time.time()
-    #         res = finish - start
-    #         print('Время работы: ', int(res))
+    def remove_progress(self, all_tasks_file, done_tasks_file):
+        os.remove(all_tasks_file)
+        os.remove(done_tasks_file)
+        logging.info('Files of progress removed')
