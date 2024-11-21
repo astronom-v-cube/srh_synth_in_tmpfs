dac9c428843ec6f8bd74d9a530d994b8c62609be
Dmitry <astronom.v.cube@gmail.com>
2024-11-22 00:44:18 +0300

    handling the raised exception

--- make_image_multiproc.py
+++ make_image_multiproc.py
@@ -7,6 +7,7 @@ import itertools
 import os
 from tqdm import tqdm
 from threading import Lock
+import sys
 
 from config import *
 
@@ -51,17 +52,31 @@ def main():
 
     remaining_tasks = [task for task in all_tasks if task not in done_tasks]
 
-    with tqdm(total=len(list(all_tasks)), desc="Progress", bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} tasks") as pbar:
-        pbar.update(len(list(done_tasks)))
+    try:
+        with tqdm(total=len(list(all_tasks)), desc="Progress", bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} tasks") as pbar:
+            pbar.update(len(list(done_tasks)))
 
-        with concurrent.futures.ProcessPoolExecutor(max_workers=num_threads) as executor:
-            future_to_task = {executor.submit(task_wrapper, task): task for task in remaining_tasks}
-            for future in concurrent.futures.as_completed(future_to_task):
-                task, success = future.result()
-                if success:
-                    done_tasks.add(task)
-                    with lock:
-                        synthesizer.save_progress(all_tasks_file, done_tasks_file, all_tasks, list(done_tasks))
-                    pbar.update(1)
+            with concurrent.futures.ProcessPoolExecutor(max_workers=num_threads) as executor:
+                try:
+                    future_to_task = {executor.submit(task_wrapper, task): task for task in remaining_tasks}
+                    for future in concurrent.futures.as_completed(future_to_task):
+                        task, success = future.result()
+                        if success:
+                            done_tasks.add(task)
+                            with lock:
+                                synthesizer.save_progress(all_tasks_file, done_tasks_file, all_tasks, list(done_tasks))
+                            pbar.update(1)
+
+                except KeyboardInterrupt:
+                        print("  <- Получен сигнал остановки. Завершаю уже запущенные процессы...")
+                        for future in future_to_task:
+                            future.cancel()
+                        executor.shutdown(wait=True)
+                        raise
+
+    except KeyboardInterrupt:
+        synthesizer.remove_srh_directories()
+        print('Остановлено вручную. Процессы завершены. RAM очищена. Дамп задач сохранен')
+        sys.exit()
 
     synthesizer.remove_progress(all_tasks_file, done_tasks_file)
